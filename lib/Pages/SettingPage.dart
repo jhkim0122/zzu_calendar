9e3cc8922a1f22ff484f84196e53f91a3cd6258f
zzuggyu <k24504@naver.com>
2022-04-30 02:01:11 +0900

    [chore] Added application information

--- lib/Pages/SettingPage.dart
+++ lib/Pages/SettingPage.dart
@@ -26,8 +26,48 @@ class _SettingPageState extends State<SettingPage>{
           backgroundColor: Colors.white,
           title: Text("Settings")
       ),
-      body: Center(child: Text("ZZU SETTING ~~~", style: TextStyle(fontWeight: FontWeight.bold))
-      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children:[
+            Center(
+                child: Container(
+                  width: 250,
+                  height: 90,
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Application Version"),
+                          Text("v0.0.1", style: TextStyle(color: Colors.grey.shade700))
+                      ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Release Date"),
+                          Text("2022.04.30", style: TextStyle(color: Colors.grey.shade700))
+                        ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Developer Contact"),
+                          Text("@zzu_run", style: TextStyle(color: Colors.grey.shade700))
+                        ]),
+                    ])
+                )
+            )
+          ]
+        )
+      )
     );
   }
 
